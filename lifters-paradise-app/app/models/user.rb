75a93997ff114ea6a5331aa8b2aa504061ed0582
Fahad Hussain <sfahadh14@gmail.com>
2019-06-03 17:16:37 -0400

    added react app

--- lifters-paradise-app/app/models/user.rb
+++ lifters-paradise-app/app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+    has_secure_password
+
     validates :first_name, presence: true
+    
     validates :last_name, presence: true
-    validates :email, presence: true
-    validates :username, presence: true
+
+    validates :username,
+    presence: true,
+    uniqueness: true,
+    length: { maximum:25 }
+
+    validates :email, 
+    presence: true,
+    uniqueness: true,
+    format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" },
+    length: { maximum:55 }
+
+    validates :password, 
+    presence: true, 
+    length: { minimum: 6 },
+    if: -> { new_record? || !password.nil? }
 end
