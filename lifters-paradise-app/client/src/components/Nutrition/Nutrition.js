f324c7c2d5f0365163a636c407fb44b89b0f322d
Fahad Hussain <sfahadh14@gmail.com>
2019-06-06 23:57:33 -0400

    designed nutritions page

--- lifters-paradise-app/client/src/components/Nutrition/Nutrition.js
+++ lifters-paradise-app/client/src/components/Nutrition/Nutrition.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar'
+import './Nutrition.css'
 
 function Nutrition() {
     return (
-        <div className="App">
+        <div id="home">
             <Navbar />
-            <h1>This is the nutrition page</h1>
-        </div>
+            <div id="intro">
+                <h1>Nutrition Tracker (Prototype)</h1>
+                <h3>Track your nutrient intake and set body composition goals</h3>
+                <button><a href="https://food-database-app.herokuapp.com">Learn More</a></button>
+            </div>
+            <h3 id="credit">Photo Credit: Cai Yang</h3>
+      </div>
     );
 }
 
