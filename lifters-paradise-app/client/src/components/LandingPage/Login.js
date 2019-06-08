9f709d37188b82d082fa3c84c815382af17f1230
Fahad Hussain <sfahadh14@gmail.com>
2019-06-07 23:46:12 -0400

    cleaned up register and login js

--- lifters-paradise-app/client/src/components/LandingPage/Login.js
+++ lifters-paradise-app/client/src/components/LandingPage/Login.js
@@ -2,24 +2,57 @@ import React from 'react';
 import { Redirect } from "react-router-dom"
 import './LandingPage.css'
 
-export default function Login(props) {
+class Login extends React.Component {
+    constructor() {
+        super()
 
-    return (
-        <div className='App'>
-            {/* <div className="wrapper">
-                <div className="container"> 
-                    <div className="login-register"> 
-                        <div className="login">Log In</div>            
-                        <div className="register" onClick={this.showRegisterForm}>Register</div>
-                    </div>  
+        this.state = {
+            login: false
+        }
+        this.showRegisterForm = this.showRegisterForm.bind(this);
+    }
 
-                    <div className="login-form">
-                        <input type="text" placeholder="Username" className="input"/>
-                        <input type="password" placeholder="Password" className="input"/>
-                        <div className="btn">log in</div>
-                    </div>   
-                </div>
-            </div>           */}
-        </div>
-    );    
+    showRegisterForm() {
+        this.setState({
+            login: true
+        })
+    }
+
+    render() {
+        const { handleChange, handleLogin, authFormData, handleLoginButton } = this.props
+        if(this.state.login) return <Redirect to="/register"/>
+
+        return (
+            <div className='App'>
+                <div className="wrapper">
+                    <div className="container"> 
+                        <div className="login-register"> 
+                            <div className="login">Log In</div>            
+                            <div className="register" onClick={this.showRegisterForm}>Register</div>
+                        </div>  
+
+                        <form className="login-form">
+                            <input 
+                                type="text" 
+                                name="username" 
+                                placeholder="Username" 
+                                className="input"
+                                onChange={this.props.handleChange}
+                            />
+
+                            <input 
+                                type="password" 
+                                name="password" 
+                                placeholder="Password" 
+                                className="input"
+                            />
+                            <div className="btn">log in</div>
+                        </form>   
+                    </div>
+                </div>          
+            </div>
+        );
+    }
 }
+
+export default Login
--- lifters-paradise-app/client/src/components/LandingPage/Register.js
+++ lifters-paradise-app/client/src/components/LandingPage/Register.js
@@ -2,24 +2,76 @@ import React from 'react';
 import { Redirect } from "react-router-dom"
 import './LandingPage.css'
 
-export default function Register(props) {
-    return (
-        <div className='App'>
-            {/* <div className="wrapper">
-                <div className="container">    
-                    <div className="login-register"> 
-                        <div className="login" onClick={this.showLoginForm}>Log In</div>              
-                        <div className="register">Register</div>
-                    </div>  
+class Register extends React.Component {
+    constructor() {
+        super()
 
-                    <div className="register-form">
-                        <input type="text" placeholder="Your Name" className="input"/>
-                        <input type="text" placeholder="Your Email Address" className="input"/>
-                        <input type="password" placeholder="Choose a Password" className="input"/>
-                        <div className="btn">Create account</div>
-                    </div>  
+        this.state = {
+            register: false
+        }
+        this.showLoginForm = this.showLoginForm.bind(this);
+    }
+
+    showLoginForm() {
+        this.setState({
+            register: true
+        })
+    }
+
+    render() {
+        if(this.state.register) return <Redirect to="/" />
+
+        return (
+            <div className='App'>
+                <div className="wrapper">
+                    <div className="container">    
+                        <div className="login-register"> 
+                            <div className="login" onClick={this.showLoginForm}>Log In</div>              
+                            <div className="register">Register</div>
+                        </div>  
+
+                        <form className="register-form">
+                            <input 
+                                type="text" 
+                                name="first_name" 
+                                placeholder="First Name" 
+                                className="input"
+                            />
+
+                            <input 
+                                type="text" 
+                                name="last_name" 
+                                placeholder="Last Name" 
+                                className="input"
+                            />
+
+                            <input 
+                                type="text" 
+                                name="username" 
+                                placeholder="username" 
+                                className="input"
+                            />
+
+                            <input 
+                                type="text" 
+                                name="email" 
+                                placeholder="Your Email Address" 
+                                className="input"
+                            />
+
+                            <input 
+                                type="password" 
+                                name="password"
+                                placeholder="Choose a Password" 
+                                className="input"
+                            />
+                            <div className="btn">Create account</div>
+                        </form>  
+                    </div>
                 </div>
-            </div> */}
-        </div>
-    );
+            </div>
+        );
+    }
 }
+
+export default Register
