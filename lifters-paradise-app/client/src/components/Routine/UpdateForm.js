434a70b0e57811fc114f5ecff9684756130d4791
Fahad Hussain <sfahadh14@gmail.com>
2019-06-12 12:21:59 -0400

    fixed update modal

--- lifters-paradise-app/client/src/components/Routine/UpdateForm.js
+++ lifters-paradise-app/client/src/components/Routine/UpdateForm.js
@@ -16,7 +16,7 @@ class WorkloadForm extends React.Component {
         return (
             <div className="App">
              <div className ="button-modal">
-                    <Modal id="modal" trigger={<Button color="blue" size="huge" id ="update-button">
+                    <Modal id="modal-routine" trigger={<Button color="blue" size="huge" id ="update-button">
                         <Icon id="edit-icon" disabled name='write'/>
                         </Button>}>
                         <h1 className="modal-style">Update Your Workout Routine</h1>
