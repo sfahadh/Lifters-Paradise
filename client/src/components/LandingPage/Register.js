e866a5357df7d0b42126172492d476d76c7fbdff
Fahad Hussain <sfahadh14@gmail.com>
2019-07-30 21:57:45 -0400

    adjusted size and margin of bluto and popeye face image

--- client/src/components/LandingPage/Register.js
+++ client/src/components/LandingPage/Register.js
@@ -24,44 +24,44 @@ class Register extends React.Component {
 
         return (
             <div className='register-background'>
-                <h1 className="title">Welcome To Lifter's Paradise</h1>
-                <div className="bluto-vs-popeye">
-                    <img id="popeye-face" src={require("./popeye.png")} alt=""/>
-                    <img id="bluto-face" src={require("./bluto.png")} alt=""/>
-                </div>
-                <div className="wrapper">
-                    <div className="container">    
-                        <div className="login-register"> 
-                            <div className="login" onClick={this.showLoginForm}>Log In</div>              
-                            <div className="register" id="color-switch">Register</div>
-                        </div>  
+                <div className="border-page">
+                    <h1 className="title">Welcome To Lifter's Paradise</h1>
+                    <div className="wrapper">
+                        <img id="popeye-face" src={require("./popeye.png")} alt=""/>
+                        <div className="container">    
+                            <div className="login-register"> 
+                                <div className="login" onClick={this.showLoginForm}>Log In</div>              
+                                <div className="register" id="color-switch">Register</div>
+                            </div>  
 
-                        <div className="register-form">
-                            <input 
-                                type="text" 
-                                name="name" 
-                                placeholder="What is your name newbie?" 
-                                className="input"
-                                onChange={handleChange}
-                            />
+                            <div className="register-form">
+                                <input 
+                                    type="text" 
+                                    name="name" 
+                                    placeholder="What is your name newbie?" 
+                                    className="input"
+                                    onChange={handleChange}
+                                />
 
-                            <input 
-                                type="text" 
-                                name="username" 
-                                placeholder="Pick another name to be called by" 
-                                className="input"
-                                onChange={handleChange}
-                            />
+                                <input 
+                                    type="text" 
+                                    name="username" 
+                                    placeholder="Pick another name to be called by" 
+                                    className="input"
+                                    onChange={handleChange}
+                                />
 
-                            <input 
-                                type="password" 
-                                name="password"
-                                placeholder="Choose a password" 
-                                className="input"
-                                onChange={handleChange}
-                            />
-                            <button onClick={handleRegisterButton} className="btn">Create account</button>
-                        </div>  
+                                <input 
+                                    type="password" 
+                                    name="password"
+                                    placeholder="Choose a password" 
+                                    className="input"
+                                    onChange={handleChange}
+                                />
+                                <button onClick={handleRegisterButton} className="btn">Create account</button>
+                            </div>  
+                        </div>
+                        <img id="bluto-face" src={require("./bluto.png")} alt=""/>
                     </div>
                 </div>
             </div>
