99fce5ec1b2531de8923278bba32b4b47e7aa1ba
Fahad Hussain <sfahadh14@gmail.com>
2019-07-31 11:49:02 -0400

    navbar can come in different directions

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,72 +1,147 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
-import { Button, Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import {
+    Button,
+    Checkbox,
+    Grid,
+    Header,
+    Icon,
+    Image,
+    Menu,
+    Segment,
+    Sidebar,
+  } from 'semantic-ui-react'
 import Modal2 from '../Modal/Modal'
 
+const HorizontalSidebar = ({ animation, direction, visible }) => (
+    <Sidebar as={Segment} animation={animation} direction={direction} visible={visible}>
+      <Grid textAlign='center'>
+        <Grid.Row columns={1}>
+          <Grid.Column>
+            <Header as='h3'>New Content Awaits</Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid columns={3} divided>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+          </Grid.Column>
+        </Grid>
+      </Grid>
+    </Sidebar>
+  )
+  
+  HorizontalSidebar.propTypes = {
+    animation: PropTypes.string,
+    direction: PropTypes.string,
+    visible: PropTypes.bool,
+  }
+  
+  const VerticalSidebar = ({ animation, direction, visible }) => (
+    <Sidebar
+      as={Menu}
+      animation={animation}
+      direction={direction}
+      icon='labeled'
+      inverted
+      vertical
+      visible={visible}
+      width='thin'
+    >
+      <Menu.Item as='a'>
+        <Icon name='home' />
+        Home
+      </Menu.Item>
+      <Menu.Item as='a'>
+        <Icon name='gamepad' />
+        Games
+      </Menu.Item>
+      <Menu.Item as='a'>
+        <Icon name='camera' />
+        Channels
+      </Menu.Item>
+    </Sidebar>
+  )
+  
+  VerticalSidebar.propTypes = {
+    animation: PropTypes.string,
+    direction: PropTypes.string,
+    visible: PropTypes.bool,
+  }
 class Navbar extends Component {
-    state = { visible: false }
-
-    handleHideClick = () => this.setState({ visible: false })
-    handleShowClick = () => this.setState({ visible: true })
-    handleSidebarHide = () => this.setState({ visible: false })
-
-    render() {
-        const { visible } = this.state
-        return (
-            <div className="App">
-                        <Button.Group>
-          <Button disabled={visible} onClick={this.handleShowClick}>
-            Show sidebar
-          </Button>
-          <Button disabled={!visible} onClick={this.handleHideClick}>
-            Hide sidebar
-          </Button>
-        </Button.Group>
-
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            icon='labeled'
-            inverted
-            onHide={this.handleSidebarHide}
-            vertical
-            visible={visible}
-            width='thin'
-          >
-            <Menu.Item as='a'>
-              <Icon name='home' />
-              Home
-            </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='gamepad' />
-              Games
-            </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='camera' />
-              Channels
-            </Menu.Item>
-          </Sidebar>
-
-          <Sidebar.Pusher dimmed={visible}>
-            <Segment basic>
-            { this.props.currentUser ? 
-                    <div id="welcome-user">
-                        <p><span>{(this.props.currentUser.username).charAt(0).toUpperCase() + this.props.currentUser.username.slice(1)}</span> Are ready to make some gainz? or </p> 
-                    </div> : <p style={{ color: "white"}}>Ready to make some Gainz ori</p> } 
-                    <div className="link-box" as='a'>
-                        <Modal2 handleLogout={this.props.handleLogout}/>
-                        <p>(logout)</p>
-                    </div>
-            </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-
+    state = {
+        animation: 'overlay',
+        direction: 'left',
+        dimmed: false,
+        visible: false,
+      }
     
+      handleAnimationChange = animation => () =>
+        this.setState(prevState => ({ animation, visible: !prevState.visible }))
+    
+      handleDimmedChange = (e, { checked }) => this.setState({ dimmed: checked })
+    
+      handleDirectionChange = direction => () => this.setState({ direction, visible: false })
+    
+      render() {
+        const { animation, dimmed, direction, visible } = this.state
+        const vertical = direction === 'bottom' || direction === 'top'
+    
+        return (
+          <div>
+            <Checkbox checked={dimmed} label='Dim Page' onChange={this.handleDimmedChange} toggle />
+    
+            <Header as='h5'>Direction</Header>
+            <Button.Group>
+              <Button active={direction === 'left'} onClick={this.handleDirectionChange('left')}>
+                Left
+              </Button>
+              <Button active={direction === 'right'} onClick={this.handleDirectionChange('right')}>
+                Right
+              </Button>
+              <Button active={direction === 'top'} onClick={this.handleDirectionChange('top')}>
+                Top
+              </Button>
+              <Button active={direction === 'bottom'} onClick={this.handleDirectionChange('bottom')}>
+                Bottom
+              </Button>
+            </Button.Group>
+    
+            <Header as='h5'>All Direction Animations</Header>
+            <Button onClick={this.handleAnimationChange('overlay')}>Overlay</Button>
+            <Button onClick={this.handleAnimationChange('push')}>Push</Button>
+            <Button onClick={this.handleAnimationChange('scale down')}>Scale Down</Button>
+    
+            <Header as='h5'>Vertical-Only Animations</Header>
+            <Button disabled={vertical} onClick={this.handleAnimationChange('uncover')}>
+              Uncover
+            </Button>
+            <Button disabled={vertical} onClick={this.handleAnimationChange('slide along')}>
+              Slide Along
+            </Button>
+            <Button disabled={vertical} onClick={this.handleAnimationChange('slide out')}>
+              Slide Out
+            </Button>
+    
+              {vertical ? (
+                <HorizontalSidebar animation={animation} direction={direction} visible={visible} />
+              ) : null}
+              {vertical ? null : (
+                <VerticalSidebar animation={animation} direction={direction} visible={visible} />
+              )}
+    
+              <Sidebar.Pusher dimmed={dimmed && visible}>
+              </Sidebar.Pusher>
           </div>
-        );
+        )
+      }
     }
-}
 
 export default Navbar
