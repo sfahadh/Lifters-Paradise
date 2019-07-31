f9242b122758913ee19f1afee84b28f7bd0b5fbb
Fahad Hussain <sfahadh14@gmail.com>
2019-07-30 21:31:47 -0400

    placed popeye and bluto image in wrapper div

--- client/src/components/LandingPage/Login.js
+++ client/src/components/LandingPage/Login.js
@@ -24,41 +24,41 @@ class Login extends React.Component {
 
         return (
             <div className='login-background'>
-                <h1 className="title">Welcome To Lifter's Paradise</h1>
-                <div className="bluto-vs-popeye">
-                    <img id="popeye" src={require("./popeye2.png")} alt=""/>
-                    <img id="bluto" src={require("./bluto2.png")} alt=""/>
-                </div>
-                <div className="wrapper">
-                    <div className="container"> 
-                        <div className="login-register"> 
-                            <div className="login" id="color-switch">Log In</div>            
-                            <div className="register" onClick={this.showRegisterForm}>Register</div>
-                        </div>  
+                <div className="border-page">
+                    <h1 className="title">Welcome To Lifter's Paradise</h1>
+                    <div className="wrapper">
+                        <img id="popeye" src={require("./popeye2.png")} alt=""/>
+                        <img id="bluto" src={require("./bluto2.png")} alt=""/>
+                        <div className="container"> 
+                            <div className="login-register"> 
+                                <div className="login" id="color-switch">Log In</div>            
+                                <div className="register" onClick={this.showRegisterForm}>Register</div>
+                            </div>  
 
-                        <div className="login-form">
-                            <input 
-                                type="text" 
-                                name="username" 
-                                placeholder="Username" 
-                                className="input"
-                                onChange={handleChange}
-                            />
+                            <div className="login-form">
+                                <input 
+                                    type="text" 
+                                    name="username" 
+                                    placeholder="Username" 
+                                    className="input"
+                                    onChange={handleChange}
+                                />
 
-                            <input 
-                                type="password" 
-                                name="password" 
-                                placeholder="Password" 
-                                className="input"
-                                onChange={handleChange}
-                            />
-                            <button 
-                                onClick={handleLoginButton}
-                                className="btn">log in
-                            </button>
-                        </div>   
-                    </div>
-                </div>          
+                                <input 
+                                    type="password" 
+                                    name="password" 
+                                    placeholder="Password" 
+                                    className="input"
+                                    onChange={handleChange}
+                                />
+                                <button 
+                                    onClick={handleLoginButton}
+                                    className="btn">log in
+                                </button>
+                            </div>   
+                        </div>
+                    </div>      
+                </div>    
             </div>
         );
     }
