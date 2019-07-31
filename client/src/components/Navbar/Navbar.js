568c64e9edb5e255f1b52895c04d6edfee6cecaf
Fahad Hussain <sfahadh14@gmail.com>
2019-07-31 16:19:20 -0400

    navbar only appears from top or bottom

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,45 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import { Button, Icon, Menu, Sidebar } from 'semantic-ui-react'
 import Modal2 from '../Modal/Modal'
   
-  const VerticalSidebar = ({ animation, direction, visible }) => (
-    <Sidebar
-      as={Menu}
-      animation={animation}
-      direction={direction}
-      icon='labeled'
-      inverted
-      vertical
-      visible={visible}
-      width='thin'
-    >
-      <Link className="link" to='home'>
-        <Icon name='home' />
-        Home
-      </Link>
-      <Link className="link" to='exercises'>
-        <Icon name='heart' />
-        Exercises
-      </Link>
-      <Link className="link" to='nutrition'>
-        <Icon name='nutritionix' />
-        Nutrition
-      </Link>
-      <Link className="link" to='routine'>
-        <Icon name='table' />
-        Routine
-      </Link>
-    </Sidebar>
-  )
-  
-  VerticalSidebar.propTypes = {
-    animation: PropTypes.string,
-    direction: PropTypes.string,
-    visible: PropTypes.bool,
-  }
 class Navbar extends Component {
     state = {
         direction: 'left',
@@ -53,6 +17,7 @@ class Navbar extends Component {
       handleDirectionChange = direction => () => this.setState({ direction, visible: false })
     
       render() {
+        const { handleLogout, currentUser } = this.props
         const { animation, direction, visible } = this.state
         const vertical = direction === 'bottom' || direction === 'top'
     
@@ -60,26 +25,55 @@ class Navbar extends Component {
           <div>
             <div className="btn-group">
                 <Button.Group>
-                    <Button className="semantic-btn" active={direction === 'left'} onClick={this.handleDirectionChange('left')}>
-                        Left
-                    </Button>
-                    <Button className="semantic-btn" active={direction === 'right'} onClick={this.handleDirectionChange('right')}>
-                        Right
-                    </Button>
                     <Button className="semantic-btn" active={direction === 'bottom'} onClick={this.handleDirectionChange('bottom')}>
                         Bottom
+                    </Button>
+                    <Button className="semantic-btn" active={direction === 'top'} onClick={this.handleDirectionChange('top')}>
+                        Top
                     </Button>
                 </Button.Group>
                 <Button className="semantic-btn semantic-toggle" onClick={this.handleAnimationChange('push')}>Toggle Navbar</Button>
             </div>
     
               {vertical ? (
-                <VerticalSidebar animation={animation} direction={direction} visible={visible} />
+                    <Sidebar
+                    as={Menu}
+                    animation={animation}
+                    direction={direction}
+                    icon='labeled'
+                    inverted
+                    vertical
+                    visible={visible}
+                    width='thin'
+                  >
+                    <Link className="link" to='home'>
+                      <Icon name='home' />
+                      Home
+                    </Link>
+                    <Link className="link" to='exercises'>
+                      <Icon name='heart' />
+                      Exercises
+                    </Link>
+                    <Link className="link" to='nutrition'>
+                      <Icon name='nutritionix' />
+                      Nutrition
+                    </Link>
+                    <Link className="link" to='routine'>
+                      <Icon name='table' />
+                      Routine
+                    </Link>
+              
+                    { this.props.currentUser ? 
+                              <div id="welcome-user">
+                                  <p><span>{(this.props.currentUser.username).charAt(0).toUpperCase() + this.props.currentUser.username.slice(1)}</span> Are ready to make some gainz? or </p> 
+                              </div> : <p>did not work</p> } 
+                              <div className="link-box" as='a'>
+                                  <Modal2 handleLogout={this.props.handleLogout}/>
+                                  <p>(logout)</p>
+                              </div>
+              
+                  </Sidebar>
               ) : null}
-              {vertical ? null : (
-                <VerticalSidebar animation={animation} direction={direction} visible={visible} />
-              )}
-
           </div>
         )
       }
