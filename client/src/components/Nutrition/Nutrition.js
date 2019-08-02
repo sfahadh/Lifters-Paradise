2863156c2fac6973ab9beef9e443935ff4d8f007
Fahad Hussain <sfahadh14@gmail.com>
2019-08-02 18:27:53 -0400

    removed credits in nutrition page

--- client/src/components/Nutrition/Nutrition.js
+++ client/src/components/Nutrition/Nutrition.js
@@ -11,7 +11,6 @@ function Nutrition(props) {
                 <h3>Track your nutrient intake and set body composition goals</h3>
                 <button><a href="https://food-database-app.herokuapp.com">Learn More</a></button>
             </div>
-            <h3 id="credit">Photo Credit: Cai Yang</h3>
       </div>
     );
 }
